8a70d37ae2a7a9ce25f1bc1080116f18a2517917
minsu <gnffk48@naver.com>
2023-12-06 07:16:29 +0900

    다른 규칙 추가해서 ai가리기

--- ai.py
+++ ai.py
@@ -10,38 +10,34 @@ import play_mode
 import server
 
 # A Run Speed
-PIXEL_PER_METER = (10.0 / 0.3)
+PIXEL_PER_METER= (10.0/0.3)
+RUN_SPEED_KMPH = 30.0
 RUN_SPEED_KMPH = 20.0
-RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
-RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
-RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+RUN_SPEED_MPM = (RUN_SPEED_KMPH*1000.0/60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM/60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS* PIXEL_PER_METER)
 # Boy Action Speed
 # fill here
 TIME_PER_ACTION = 1
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 11
-
-animation_names = ['attack_middle', 'attack_up', 'defence',
-                   'Idle', 'run', 'run_back', 'score']
-
-
+animation_names = ['attack_middle', 'attack_up','defence',
+                   'Idle','run','run_back','score']
 class AI:
     images = None
-
     def load_images(self):
         if AI.images == None:
             AI.images = {}
             for name in animation_names:
-                AI.images[name] = [load_image("resource/" + name + ".png")]
-
+                AI.images[name] = [load_image("resource/" + name + ".png") ]
     def __init__(self):
         self.x = 980
         self.y = 150
-        self.weapon_x, self.weapon_y = 850, 330
+        self.weapon_x, self.weapon_y = 850, 230
         self.size_x = 500
         self.size_y = 348
         self.load_images()
-        self.dir = 0.0  # radian 값으로 방향을 표시
+        self.dir = 0.0      # radian 값으로 방향을 표시
         self.speed = 0.0
         self.frame = 0
         self.state = 'Idle'
@@ -51,185 +47,127 @@ class AI:
         self.tx, self.ty = 0, 0
         self.build_behavior_tree()
         self.count = 0
-        self.collision_checked = False
-        self.score_incremented = False  # 초기화
-
     def get_bb(self):
-        return self.x + 30, self.y - 100, self.x + 110, self.y + 90  # 충돌 박스 크기 x = 90 / y = 190
-
+        return self.x + 30, self.y - 100, self.x + 110, self.y + 90 #충돌 박스 크기 x = 90 / y = 190
     def get_aa(self):
-        return self.weapon_x, self.weapon_y, self.weapon_x + 14, self.weapon_y + 14
-
+        return self.weapon_x, self.weapon_y, self.weapon_x+ 14, self.weapon_y + 14
     def update(self):
         self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % FRAMES_PER_ACTION
         self.bt.run()
-        # print('방어')
-        # print(self.defence_cooldown)
-        # print('위공격')
-        # print(self.attack_up_cooldown)
-        # print('아래')
-        # print(self.attack_middle_cooldown)
-        print('ai')
-        print(self.state)
-        print('나')
-        print(server.hero.state)
-        if self.defence_cooldown > 0:
-            self.defence_cooldown -= game_framework.frame_time
-            if self.defence_cooldown < 0:
-                self.defence_cooldown = 0
-
-        if self.attack_up_cooldown > 0:
-            self.attack_up_cooldown -= game_framework.frame_time
-            if self.attack_up_cooldown < 0:
-                self.attack_up_cooldown = 0
-
-        if self.attack_middle_cooldown > 0:
-            self.attack_middle_cooldown -= game_framework.frame_time
-            if self.attack_middle_cooldown < 0:
-                self.attack_middle_cooldown = 0
-
-
-
         if self.x > 1600 - 90:
             self.x = 1600 - 90
-
+            self.weapon_x = self.x - 120
+            self.weapon_y = self.y + 80
     def draw(self):
+        sx, sy = self.x - server.background.window_left, self.y - server.background.window_bottom
         image = AI.images[self.state][0]  # 가져온 이미지 리스트에서 첫 번째 이미지를 사용
-
         image.clip_draw(int(self.frame) * 500, 0 * 348, 500, 348, self.x, self.y)
         draw_rectangle(*self.get_bb())
         draw_rectangle(*self.get_aa())
         if self.x - server.hero.x < 100:
             self.x = server.hero.x + 100
-            self.weapon_x = self.x - 130
-            self.weapon_y = self.y + 180
-
+            self.weapon_x = self.x - 120
+            self.weapon_y = self.y + 80
     def handle_event(self, event):
         pass
-
+    def handle_collision(self, group, other):
+        pass
     def score_not_equal_condition(self):
         if server.score.ai_score != server.score.hero_score:
             return BehaviorTree.SUCCESS
         else:
             return BehaviorTree.FAIL
-        pass  # c1
-
+        pass #c1
     def score_less_score_condition(self):
         if server.score.ai_score < server.score.hero_score:
             return BehaviorTree.SUCCESS
         else:
             return BehaviorTree.FAIL
-        pass  # c2
-
+        pass #c2
     def score_not_meet_condition(self):
-        if self.x - server.hero.x > 100:
+        if self.x- server.hero.x > 100:
             return BehaviorTree.SUCCESS
         else:
             return BehaviorTree.FAIL
-        pass  # c3
-
+        pass #c3
     def score_meet_condition(self):
-        if self.x - server.hero.x <= 100:
+        if self.x- server.hero.x <= 100:
             return BehaviorTree.SUCCESS
         else:
             return BehaviorTree.FAIL
-        pass  # c4
-
+        pass #c4
     def ai_defecne_time_up_condition(self):
-        if server.hero.state == 'IDLE':
+        if self.defence_cooldown >0:
             return BehaviorTree.SUCCESS
         else:
             return BehaviorTree.FAIL
-        pass  # c5
-
+        pass #c5
     def ai_defecne_time_zero_condition(self):
-        if server.hero.state != 'IDLE':
+        if self.defence_cooldown ==0:
             return BehaviorTree.SUCCESS
         else:
             return BehaviorTree.FAIL
-        pass  # c6
-
+        pass #c6
     def hero_attack_condition(self):
-        if server.hero.state == 'ATTACK':
-                return BehaviorTree.SUCCESS
-        else:
-
-            return BehaviorTree.FAIL
-        pass  # c7
-
-    def ai_attack_time_zero_condition(self):
-        if server.hero.state not in 'ATTACK':
+        if server.hero.state == 'Attack':
             return BehaviorTree.SUCCESS
         else:
             return BehaviorTree.FAIL
-        pass  # c8
-
-    def ai_one_attack_time_zero_condition(self):
-        if server.hero.state == 'IDLE':
-                return BehaviorTree.SUCCESS
+        pass #c7
+    def ai_attack_time_zero_condition(self):
+        if self.attack_up_cooldown == 0 or self.attack_middle_cooldown ==0:
+            return BehaviorTree.SUCCESS
         else:
             return BehaviorTree.FAIL
-        pass  # c9
-
+        pass #c8
+    def ai_one_attack_time_zero_condition(self):
+        if (self.attack_up_cooldown != 0 and self.attack_middle_cooldown ==0) or (self.attack_up_cooldown == 0 and self.attack_middle_cooldown !=0):
+            return BehaviorTree.SUCCESS
+        else:
+            return BehaviorTree.FAIL
+        pass #c9
     def score_more_score_condition(self):
         if server.score.ai_score > server.score.hero_score:
             return BehaviorTree.SUCCESS
         else:
             return BehaviorTree.FAIL
-        pass  # c10
-
+        pass #c10
     def distance_less_than(self):
         if self.x - server.hero.x < 350:
             return BehaviorTree.SUCCESS
         else:
             return BehaviorTree.FAIL
-        pass  # c11
-
+        pass #c11
     def distance_more_than(self):
         if self.x - server.hero.x >= 350:
             return BehaviorTree.SUCCESS
         else:
             return BehaviorTree.FAIL
-        pass  # c12
-
+        pass #c12
     def score_more_score_condition(self):
         if server.score.ai_score > server.score.hero_score:
             return BehaviorTree.SUCCESS
         else:
             return BehaviorTree.FAIL
-        pass  # c13
-
+        pass #c13
     def hero_attack_time_zero_condition(self):
-        if server.hero.attack_up_cooldown == 0 or server.hero.attack_middle_cooldown == 0:
+        if server.hero.attack_up_cooldown ==0 or server.hero.attack_middle_cooldown==0:
             return BehaviorTree.SUCCESS
         else:
             return BehaviorTree.FAIL
-        pass  # c14
-
+        pass #c14
     def hero_attack_time_zero_not_condition(self):
-        if server.hero.attack_up_cooldown != 0 or server.hero.attack_middle_cooldown != 0:
+        if server.hero.attack_up_cooldown !=0 or server.hero.attack_middle_cooldown!=0:
             return BehaviorTree.SUCCESS
         else:
             return BehaviorTree.FAIL
-        pass  # c15
-
+        pass #c15
     def score_equal_conditon(self):
         if server.score.ai_score == server.score.hero_score:
             return BehaviorTree.SUCCESS
         else:
             return BehaviorTree.FAIL
-        pass  # c16
-
-    def ai_up_attack_time_zero_condition(self):
-        probability = random.random()
-
-        # Set the success threshold to 0.7 (70%)
-        success_threshold = 0.7
-        if probability < success_threshold:
-            return BehaviorTree.SUCCESS
-        else:
-            return BehaviorTree.FAIL
-        pass  # c17
+        pass #c16
 
     def front_Move(self):
         if self.state != 'run':
@@ -284,13 +222,13 @@ class AI:
             self.state = 'attack_middle'
 
             pass
-
-        if self.frame <= 5.5:
-            self.weapon_x -= RUN_SPEED_PPS * game_framework.frame_time / 2
-            pass
-        elif self.frame > 5.5:
-            self.weapon_x += RUN_SPEED_PPS * game_framework.frame_time / 2
-            pass
+        if server.score.score_state_ai == False:
+            if self.frame <= 5.5:
+                self.weapon_x -= RUN_SPEED_PPS * game_framework.frame_time / 2
+                pass
+            elif self.frame > 5.5:
+                self.weapon_x += RUN_SPEED_PPS * game_framework.frame_time / 2
+                pass
         if self.frame >= 10.9:
             self.frame = 0
             self.state = 'Idle'
@@ -305,13 +243,13 @@ class AI:
             self.state = 'attack_up'
 
             pass
-
-        if self.frame <= 5.5:
-            self.weapon_x -= RUN_SPEED_PPS * game_framework.frame_time / 2
-            pass
-        elif self.frame > 5.5:
-            self.weapon_x += RUN_SPEED_PPS * game_framework.frame_time / 2
-            pass
+        if server.score.score_state_ai == False:
+            if self.frame <= 5.5:
+                self.weapon_x -= RUN_SPEED_PPS * game_framework.frame_time / 2
+                pass
+            elif self.frame > 5.5:
+                self.weapon_x += RUN_SPEED_PPS * game_framework.frame_time / 2
+                pass
 
         if self.frame >= 10.9:
             self.frame = 0
@@ -333,113 +271,95 @@ class AI:
         else:
             return BehaviorTree.RUNNING
         pass  # a6
-
     def handle_collision(self, group, other):
         if group == 'ai:hero':
-            if server.hero.state != 'DEFENCE':  # defence 일때는 공격 추가 안됨
+            if server.ai.state != 'defence': #defence 일때는 공격 추가 안됨
                 server.score.score_state_ai = True
                 server.score.ai_score += 1
                 self.x = server.hero.x + 100
-                self.weapon_x = self.x - 130
-                self.weapon_y = self.y + 180
-                # print(self.count)
+                self.weapon_x = self.x - 120
+                self.weapon_y = self.y + 80
 
-            # print("찌름")
+            #print("찌름")
             pass
-
     def build_behavior_tree(self):
         c1 = Condition('점수가 안똑같음?', self.score_not_equal_condition)
         c2 = Condition('점수가 적음?', self.score_less_score_condition)
         c3 = Condition('못만남?', self.score_not_meet_condition)
-        c4 = Condition('만남?', self.score_meet_condition)
-        c5 = Condition('상대 상태가 IDLE인가?', self.ai_defecne_time_up_condition)
-        c6 = Condition('상대 상태가 IDLE이 아닌가', self.ai_defecne_time_zero_condition)
-        c7 = Condition('상대방이 공격중이면서 방어 쿨타임이 0인가?', self.hero_attack_condition)
-        c8 = Condition('상대가 공격중이 아니거나 나의 방어 쿨타임이 0이 아닌가?', self.ai_attack_time_zero_condition)
-        c9 = Condition('ai 중단 쿨타임이 0인가', self.ai_one_attack_time_zero_condition)
+        c4 = Condition('만남?',self.score_meet_condition )
+        c5 = Condition('방어 쿨타임 0이상인가?',self.ai_defecne_time_up_condition)
+        c6 = Condition('방어 쿨타임이 0인가',self.ai_defecne_time_zero_condition)
+        c7 = Condition('상대방이 공격중인가?', self.hero_attack_condition)
+        c8 = Condition('둘중에 하나라도 AI 공격쿨타임이 0인가?', self.ai_attack_time_zero_condition)
+        c9 = Condition('AI공격쿨타임이 하나만 0인가', self.ai_one_attack_time_zero_condition)
         c10 = Condition('점수가 많음?', self.score_more_score_condition)
         c11 = Condition('일정거리 안으로 왔는가?', self.distance_less_than)
-        c12 = Condition('일정거리 밖에 있는가?', self.distance_more_than)
-        c13 = Condition('점수가 더 많은가?', self.score_more_score_condition)
+        c12= Condition('일정거리 밖에 있는가?', self.distance_more_than)
+        c13= Condition('점수가 더 많은가?', self.score_more_score_condition)
         c14 = Condition('상대 공격 쿨타임이 0인가?', self.hero_attack_time_zero_condition)
         c15 = Condition('상대 공격 쿨타임이 0이 아닌가?', self.hero_attack_time_zero_not_condition)
         c16 = Condition('점수가 똑같은가?', self.score_equal_conditon)
-        c17 = Condition('ai 상단공격 쿨타임이 0인가 ', self.ai_up_attack_time_zero_condition)
-        a1 = Action('앞으로가기', self.front_Move)  # 앞으로 이동
+        a1 = Action('앞으로가기', self.front_Move) #앞으로 이동
         a2 = Action('뒤로가기', self.back_Move)
         a3 = Action('방어하기', self.defence)
         a4 = Action('중단 공격하기', self.middle_attack)
         a5 = Action('상단 공격하기', self.up_attack)
         a6 = Action('IDLE', self.idle)
-
         ##
         SEQ_MIDDLE_ATTACK = Sequence('MIDDLE_ATTACK', c9, a4)
-        SEQ_UP_ATTACK = Sequence('UP_ATTACK', c17, a5)
-
-        SEQ_UP_ATTACK_OR_IDLE = Selector('UP_ATTACK_OR_IDLE', SEQ_UP_ATTACK, a6)
-        SEL_UP_ATTACK_MIDDLE_ATTACK = Selector('UP_ATTACK_MIDDLE_ATTACK', SEQ_MIDDLE_ATTACK, SEQ_UP_ATTACK_OR_IDLE)
+        SEL_UP_ATTACK_MIDDLE_ATTACK = Selector('UP_ATTACK_MIDDLE_ATTACK', SEQ_MIDDLE_ATTACK, a5)
         ##
-
         ##
-        SEQ_TWO_ATTACK = Sequence('Two_Attack', c8, a4)
+        SEQ_TWO_ATTACK = Sequence('Two_Attack', c8, SEL_UP_ATTACK_MIDDLE_ATTACK)
         SEQ_DEFENCE = Sequence('DEFENCE', c7, a3)
         ##
-
         ##
         SEL_ATTACK_AND_DEFENCE = Selector('ATTACK_AND_DEFENCE', SEQ_DEFENCE, SEQ_TWO_ATTACK)
         ##
-        # c7 ,c9,
         ##
         SEQ_BEHAVIOR = Sequence('BEHAVIOR', c6, SEL_ATTACK_AND_DEFENCE)
-        SEQ_NOT_BEHAVIOR = Sequence('NOT_BEHAVIOR', c5, a6)
+        SEQ_BACK = Sequence('BACK', c5, a2)
         ##
-
         ##
-        SEL_BEHAVIOR_AND_BACK = Selector('BEHAVIOR_AND_BACK', SEQ_NOT_BEHAVIOR, SEQ_BEHAVIOR)
+        SEL_BEHAVIOR_AND_BACK = Selector('BEHAVIOR_AND_BACK', SEQ_BACK, SEQ_BEHAVIOR)
         ##
-        # *
-        SEQ_GO_MORE_SCORE = Sequence('GO_MORE_SCORE', c15, a1)
-        SEQ_BACK_MORE_SCORE = Sequence('BACK_MORE_SCORE', c14, a2)
-        # *
+        #*
+        SEQ_GO_MORE_SCORE = Sequence('GO_MORE_SCORE',c15,a1)
+        SEQ_BACK_MORE_SCORE = Sequence('BACK_MORE_SCORE',c14,a2)
+        #*
         ##
         SEQ_NOT_MEET = Sequence('NOT_MEET', c3, a1)
         SEQ_MEET = Sequence('MEET', c4, SEL_BEHAVIOR_AND_BACK)
         ##
-
-        # *
+        #*
         SEL_GO_AND_BACK = Selector('GO_AND_BACK', SEQ_GO_MORE_SCORE, SEQ_BACK_MORE_SCORE)
         SEQ_NOT_MEET_SCORE_MORE = Sequence('NOT_MEET_SCORE_MORE', c3, SEL_GO_AND_BACK)
-        # *
-
+        #*
         ##
-        SEL_MEET_OR_NOT_MEET = Selector('MEET_OR_NOT_MEET', SEQ_NOT_MEET, SEQ_MEET)
+        SEL_MEET_OR_NOT_MEET = Selector('MEET_OR_NOT_MEET',SEQ_NOT_MEET ,SEQ_MEET)
         SEQ_DISTANCE_LESS = Sequence('Distance_less', c11, SEL_MEET_OR_NOT_MEET)
         SEQ_DISTANCE_MORE = Sequence('Distance_more', c12, a1)
         ##
-
-        # *
+        #*
         SEL_MEET_OR_NOT_MEET_SCORE_MORE = Selector('MEET_OR_NOT_MEET_SCORE_MORE', SEQ_NOT_MEET_SCORE_MORE, SEQ_MEET)
         SEQ_DISTANCE_LESS_SCORE_MORE = Sequence('DISTANCE_MORE_SCORE_MORE', c11, SEL_MEET_OR_NOT_MEET_SCORE_MORE)
-        # *
-
+        #*
         ##
         SEL_DISTANCE_LESS_AND_MORE = Selector('DISTANCE_LESS_AND_MORE', SEQ_DISTANCE_LESS, SEQ_DISTANCE_MORE)
         SEQ_SCORE_LESS = Sequence('LESS_SCORE', c2, SEL_DISTANCE_LESS_AND_MORE)
         ##
-
-        # *
-        SEL_DISTANCE_LESS_AND_MORE_SCORE_MORE = Selector('DISTANCE_LESS_AND_MORE(SCORE_MORE)',
-                                                         SEQ_DISTANCE_LESS_SCORE_MORE, a6)
+        #*
+        SEL_DISTANCE_LESS_AND_MORE_SCORE_MORE = Selector('DISTANCE_LESS_AND_MORE(SCORE_MORE)', SEQ_DISTANCE_LESS_SCORE_MORE, a6)
         SEQ_SCORE_MORE = Sequence('SCORE_MORE', c13, SEL_DISTANCE_LESS_AND_MORE_SCORE_MORE)
-        # *
+        #*
 
-        # &
+        #&
+        SEQ_EQUAL = Sequence('EQUAL', c16, a5)
         SEQ_EQUAL = Sequence('EQUAL', c16, SEL_DISTANCE_LESS_AND_MORE)
-        # SEQ_EQUAL = Sequence('EQUAL', c16, a6)#테스트
-        # &
+        #&
         ##
         SEL_SCORE_MORE_AND_LESS = Selector('SCORE_MORE_AND_LESS', SEQ_SCORE_LESS, SEQ_SCORE_MORE)
         SEQ_NOT_EQUAL = Sequence('NOT_EQUAL', c1, SEL_SCORE_MORE_AND_LESS)
         ##
-        root = SEL_MIDDLE_SCORE_OR_NOT = Selector('MIDDLE_SCORE_OR_NOT', SEQ_NOT_EQUAL, SEQ_EQUAL)
+        root =  SEL_MIDDLE_SCORE_OR_NOT = Selector('MIDDLE_SCORE_OR_NOT', SEQ_NOT_EQUAL, SEQ_EQUAL)
         self.bt = BehaviorTree(root)
